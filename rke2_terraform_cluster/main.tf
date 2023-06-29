75d1ff3e58eb4a85524732e85b36210bc32a624c
Siva Kanakala <siva.kanakala@suse.com>
2023-06-29 08:44:09 +0530

    RKE2_Terraform_Cluster_Script

---
+++ rke2_terraform_cluster/main.tf
@@ -0,0 +1,51 @@
+# Required providers
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "~>3.5.1"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "~>2.2.0"
+    }
+    libvirt = {
+      source = "dmacvicar/libvirt"
+      version = "~> 0.7.1"
+    }
+  }
+}
+
+provider "libvirt" {
+  # Configuration options
+  uri = "qemu:///system"
+}
+
+
+# A pool for all cluster volumes
+resource "libvirt_pool" "rke2_cluster_pool" {
+  name = "rke2_cluster_pool"
+  type = "dir"
+  path = var.rke2_cluster_storage_pool_path
+}
+
+# Base OS image to use to create a cluster of different nodes
+resource "libvirt_volume" "os_image_ubuntu" {
+  name   = "os_image_ubuntu"
+  pool   = libvirt_pool.rke2_cluster_pool.name
+  source = var.rke2_os_image_path
+  format = "qcow2"
+}
+
+resource "libvirt_network" "kube_network" {
+   name = "k8s_network"
+   mode = "nat"
+   addresses = ["192.168.122.0/24"]
+   autostart = true
+   dhcp {
+      enabled = false
+   }
+   dns {
+     enabled = true
+   }
+}
---
+++ rke2_terraform_cluster/terraform.tfvars
@@ -0,0 +1,16 @@
+rke2_os_image_path = "/home/skanakala/Downloads/mylab/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
+rke2_cluster_storage_pool_path = "/home/skanakal/Downloads/rke2_cluster_storage"
+
+rke2_join_token = "secrettoken"
+
+rke2_server_ips = ["192.168.122.151"]
+rke2_server_vcpu = 4
+rke2_server_memory = 4096
+rke2_server_disk_size = "26843545600"     # 1024 * 1024 * 1024 * 25 (Bytes)
+
+rke2_add_server_ips = ["192.168.122.152","192.168.122.153"]
+
+rke2_agent_ips = ["192.168.122.154"]
+agent_vcpu = 2
+agent_memory = 2048
+agent_disk_size =  "26843545600"     #  1024 * 1024 * 1024 * 25 (Bytes)
